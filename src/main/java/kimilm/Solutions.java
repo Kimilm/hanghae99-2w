3f0cf75305779940960d704ae5c2beb7beddfa73
Kimilm <gnlfla7416@gmail.com>
2022-07-15 11:10:35 +0900

    추가 - 프로젝트 기본 폴더

---
+++ src/main/java/kimilm/Solutions.java
@@ -0,0 +1,8 @@
+package kimilm;
+
+public class Solutions {
+    public String _2016년(int a, int b) {
+        System.out.println("HELLO");
+        return "";
+    }
+}
---
+++ src/test/java/kimilm/SolutionsTest.java
@@ -0,0 +1,16 @@
+package kimilm;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionsTest {
+
+    private Solutions solution = new Solutions();
+
+    @Test
+    void _2016년() {
+        solution._2016년(1, 2);
+    }
+}
