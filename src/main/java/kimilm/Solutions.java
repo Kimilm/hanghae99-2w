bba65e22b15c53dab1e325931d46d245e3a75764
Kimilm <gnlfla7416@gmail.com>
2022-07-15 11:34:05 +0900

    2016년 풀이

--- src/main/java/kimilm/Solutions.java
+++ src/main/java/kimilm/Solutions.java
@@ -2,7 +2,17 @@ package kimilm;
 
 public class Solutions {
     public String _2016년(int a, int b) {
-        System.out.println("HELLO");
-        return "";
+        int[] month = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = new String[]{"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+
+        int days = b;
+
+        for (int i = 0; i < a; ++i) {
+            days += month[i];
+        }
+
+        days %= 7;
+
+        return week[days];
     }
 }
--- src/test/java/kimilm/SolutionsTest.java
+++ src/test/java/kimilm/SolutionsTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionsTest {
-
     private Solutions solution = new Solutions();
 
     @Test
     void _2016년() {
-        solution._2016년(1, 2);
+        String result = solution._2016년(5, 24);
+        System.out.println(result);
     }
 }
